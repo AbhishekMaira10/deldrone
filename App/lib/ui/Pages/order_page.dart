b7ebc15b9466e4d786f21e9dbc512c87d0d24597
AbhishekMaira10 <abhishekmaira1999@gmail.com>
2020-07-03 23:56:28 +0530

    added orders page

--- App/lib/ui/Pages/order_page.dart
+++ App/lib/ui/Pages/order_page.dart
@@ -1,3 +1,5 @@
+import 'package:deldrone_customer/custom_widgets/custom_text.dart';
+import 'package:deldrone_customer/custom_widgets/ui_widgets/order_card.dart';
 import 'package:flutter/material.dart';
 
 class OrderPage extends StatefulWidget {
@@ -8,8 +10,143 @@ class OrderPage extends StatefulWidget {
 class _OrderPageState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Order Page"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: CustomText(
+          text: 'Your Food Cart',
+          color: Colors.black,
+          family: 'Circular',
+          weight: FontWeight.bold,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          OrderCard(),
+          OrderCard(),
+        ],
+      ),
+      bottomNavigationBar: _buildTotalContainer(),
+    );
+  }
+
+  Widget _buildTotalContainer() {
+    return Container(
+      height: 220.0,
+      padding: EdgeInsets.only(
+        left: 10.0,
+        right: 10.0,
+      ),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Subtotal",
+                style: TextStyle(
+                    color: Color(0xFF9BA7C6),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Rs 300.0",
+                style: TextStyle(
+                    color: Color(0xFF6C6D6D),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Discount",
+                style: TextStyle(
+                    color: Color(0xFF9BA7C6),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Rs 30.0",
+                style: TextStyle(
+                    color: Color(0xFF6C6D6D),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Divider(
+            height: 2.0,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Cart Total",
+                style: TextStyle(
+                    color: Color(0xFF9BA7C6),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Rs 270.0",
+                style: TextStyle(
+                    color: Color(0xFF6C6D6D),
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 50.0,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(35.0),
+              ),
+              child: Center(
+                child: Text(
+                  "Proceed To Checkout",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+        ],
+      ),
     );
   }
 }
